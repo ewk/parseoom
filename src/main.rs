57488eba23933b07b7d80d48ddc643446e5d67bd
Eddie Kovsky <ewk@edkovsky.org>
2021-10-31 13:34:10 -0600

    Improve error messages

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,24 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut args = env::args();
 
     if args.len() < 2 {
+        eprintln!("USAGE: oomparse [filename]");
         process::exit(1);
     }
 
     args.next();
 
-    let filename = match args.next() {
-        Some(arg) => arg,
-        None => ("Didn't get a file name".to_string()),
-    };
+    let filename = args.next().expect("Didn't get a filename");
 
     // read in only from beginning of last oom kill to end of report
     // TODO doesn't read the *last* oom messages
-    let contents = fs::read_to_string(filename).unwrap();
+    let contents = fs::read_to_string(filename).expect("Could not read filename");
 
-    let re = Regex::new(r"(?s)\w+\sinvoked oom-killer.*Out of memory: Kill process.*child").unwrap();
-    let mat = re.captures(&contents).unwrap();
-    let oom = mat.get(0).unwrap().as_str().lines();
+    let re = Regex::new(r"(?s)\w+\sinvoked oom-killer.*Out of memory: Kill process.*child")
+        .unwrap();
+    let mat = re.captures(&contents).ok_or("Could not find an oom kill message in this file")?;
+    let oom = mat.get(0).expect("Match for 'invoked oom-killer' not found")
+        .as_str()
+        .lines();   // convert match to a str iterator
 
     let re = Regex::new(r"(.*)?kernel:\s*(\[\d+\.\d+\])?\s").unwrap();
     let mut cleaned = String::new();
